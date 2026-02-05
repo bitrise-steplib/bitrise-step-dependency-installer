0405a3a335dff934dba665869c6d7449cec57a3b
Ben Boral <ben.boral@bitrise.io>
2026-02-05 15:08:52 -0600

    input validation

--- main.go
+++ main.go
@@ -4,20 +4,26 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/bitrise-io/bitrise-step-dependency-installer/installer"
 )
 
 func main() {
 	toolVersionFile := os.Getenv("tool_version_file")
+	workflow := os.Getenv("bitrise_workflow_id")
+	verboseMode := os.Getenv("verbose") == "true"
 
 	if toolVersionFile == "" {
 		fmt.Println("No tool version file provided in 'tool_version_file' environment variable.")
 		os.Exit(1)
+	} else if len(strings.Split(toolVersionFile, " ")) > 1 {
+		fmt.Println("Multiple tool version files are not supported.")
+		os.Exit(1)
+	} else if len(strings.Split(workflow, " ")) > 1 {
+		fmt.Println("Multiple workflows are not supported.")
+		os.Exit(1)
 	}
-
-	workflow := os.Getenv("bitrise_workflow_id")
-	verboseMode := os.Getenv("verbose") == "true"
 
 	output, err := installer.Install(toolVersionFile, workflow, verboseMode, exec.Command)
 
